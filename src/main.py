014e33c82260d9e3f3347e1caf8b32ad52845209
Karl N <nordenk@protonmail.com>
2021-05-21 15:28:03 -0700

    modified the code to produce a 100% deterministic behavior

--- src/main.py
+++ src/main.py
@@ -1,32 +1,28 @@
-"""session module
+"""session module -- DO NOT USE THIS MODULE AS AN IMPORT
 """
 from backend.menu import Menu
 
-# start a new session:
+
 def session(this_session: bool = True):
     while this_session is True:
         accounts = dict()       # list of accounts created for this session
         interact = Menu()       # start a new interactive api object
 
         in_main_menu = this_session
-        # while in main menu
         while in_main_menu:
             u_input = interact.main_ui()
-
+            # first menu option:
             if u_input == '1':
                 u, accounts = interact.main_create(accounts)
 
+            # second menu option:
             elif u_input == '2':
                 is_logged, u = not in_main_menu, None
-
                 # loop back if incorrect id or pin
                 while not is_logged:
                     is_logged, u = interact.main_attempt_login(accounts)
-
-                # otherwise, log in:
                 else:
                     in_user_menu = is_logged
-                    # while logged in
                     while in_user_menu:
                         u_input = interact.user_ui()
                         if u_input == '1':
@@ -36,13 +32,20 @@ def session(this_session: bool = True):
                             in_user_menu = False
                         elif u_input == '0':
                             interact.user_exit()
+
+            # third menu option:
             elif u_input == '0':
                 interact.user_exit()
-            else:
+
+            else:  # otherwise just loop back
                 continue
     else:
-        print(f"<this_session == {this_session}>. Session not started.")
+        print(f"\nWarning: session not started.\n")
+        print(f"<this_session == {this_session}>, when supposed to be <True>. Perhaps used as an import?")
 
 
+# only execute when this module is run directly
 if __name__ == "__main__":
-    session()
+    session(__name__ == "__main__")
+else:
+    session(__name__ == "__main__")
